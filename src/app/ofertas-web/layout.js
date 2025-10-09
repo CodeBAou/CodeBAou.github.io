e35bc78b46be7faa826e59d5631cbf067c82eaa3
CodeBAou <borisafou@gmail.com>
2025-10-09 13:45:53 +0200

    cambio url imagen favicon para el buscador

--- src/app/ofertas-web/layout.js
+++ src/app/ofertas-web/layout.js
@@ -3,7 +3,7 @@
 export const metadata = {
   title: 'DWBA █ Creamos tu pagina web con SEO y Mantenimiento incluidos. ★VER OFERTA★ ',
   description: 'Es una oferta limitada para nuevos clientes. Un paquete que te permite ahorrar dinero en servicios de seo y mantenimiento.',
-  icons: '/icon/LOGO_DWBA_simbolo_32.webp'
+  icons: 'https://dwba.es/icon/LOGO_DWBA_simbolo_48.png'
 }
 
 export default function RootLayout({ children }) {
