fd1d6cfb12e2cccee58feeb5c2a5e03d38cd0ef8
CodeBAou <borisafou@gmail.com>
2025-10-09 12:26:03 +0200

    cambio favicon a png

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,19 +7,15 @@ import './main.css';
 export const metadata = {
   title: 'DWBA █ Diseño y desarrollo web profesional en Ourense y Pontevedra ',
   description: 'Creamos páginas web profesionales y estrategias digitales para negocios en Ourense, Pontevedra y alrededores. Aumenta tu visibilidad online. Cortegada | A Cañiza | Celanova | Ourense provincia.',
-  icons: '/icon/LOGO_DWBA_simbolo_32.webp'
+  icons: '/icon/LOGO_DWBA_simbolo_48.png'
 }
 
 export default function RootLayout({ children }) {
-
-
 
   return (
     <html >
       <head>
       
-       
-        
         <link
           rel="canonical"
           href="https://dwba.es/"
