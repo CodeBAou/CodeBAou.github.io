9e2994f3266f300b0997bcd22e1f0657cd797a97
CodeBAou <borisafou@gmail.com>
2025-11-05 20:34:30 +0100

    optimizaciones de los textos y contacto de la pagina de inicio

--- src/componentes/section_header/Section_header.jsx
+++ src/componentes/section_header/Section_header.jsx
@@ -6,8 +6,8 @@ export default function Section_header(){
     return(
        <div className="section-header">
             <img className="logoDWBA" src="/image/logo_DWBA_sin_fondo.webp" alt="Enlace de una oferta para crear una pagina web desde 31 euros a 123 euros (tienda online)"/>
-            <h1 id="dwba">DWBA ,DISEÑO WEB, DESARROLLO Y MARKETING DIGITAL</h1>
-            <p>Ourense y Pontevedra</p>
+            <h1 id="dwba">DISEÑO WEB Y MARKETING DIGITAL</h1>
+            <p>DWBA, Ourense</p>
             <div className="apps_list">
                 <img className="app_imagen" src="/image/Logo-de-la-app-pixelgrino-de-DWBA.png" alt="El logo de la app pixelgrino de DWBA" />
             </div>
