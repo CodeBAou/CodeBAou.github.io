8bbd0d14f602264ec3918c65ec39156c09ade788
CodeBAou <borisafou@gmail.com>
2025-11-05 21:05:44 +0100

    se añaden las paginas legales de cookies y privacidad

---
+++ src/app/politica-de-cookies/page.jsx
@@ -0,0 +1,65 @@
+"use client";
+
+
+import './terminosycondiciones.css';
+
+export default function Page(){
+
+    return(
+        <div className="page_legal">
+          <h1>Política de Cookies</h1>
+          <section>
+            <h2>Información general</h2>
+            <ul>
+                <li><b>Nombre comercial:</b> DWBA</li>
+                <li><b>Titular:</b>Boris Álvarez Fernández</li>
+                <li><b>Localidad:</b> Cortegada, Ourense </li>
+                <li><b>Email:</b> dwba.soporte@gmail.com </li>
+            </ul>
+          </section>
+          <section>
+                <article>
+                    <h2>1. Qué son las cookies</h2>
+                    <p>Las cookies son pequeños archivos que se guardan en tu dispositivo al visitar una web. Sirven para recordar tus preferencias, analizar el uso del sitio o mejorar tu experiencia de navegación.</p>
+                </article>
+                <article>
+                    <h2>2. Qué tipos de cookies usamos</h2>
+                    <p>DWBA utiliza cookies propias y de terceros con las siguientes finalidades:</p>
+                    <ul>
+                        <li><b>Cookies técnicas o necesarias</b>: permiten el funcionamiento básico del sitio (por ejemplo, recordar tu idioma o si aceptaste las cookies).</li>
+                        <li><b>Cookies de análisis</b>: nos ayudan a entender cómo los usuarios usan la web para mejorar su contenido y rendimiento.</li>
+                        <li>
+                            <ul>
+                                <li>En concreto, usamos <b>Google Analytics</b> para obtener estadísticas anónimas sobre las visitas y el uso del sitio.</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </article>
+                <article>
+                    <h2>3. Cómo puedes gestionar las cookies</h2>
+                    <p>Además, puedes borrar o bloquear las cookies desde la configuración de tu navegador:</p>
+                    <ul>
+                        <li>Google Chrome:   <a href="https://support.google.com/chrome/answer/95647">Configuración de cookies</a></li>
+                        <li>Mozilla Firefox: <a href="https://support.mozilla.org/es/kb/habilitar-y-deshabilitar-cookies-sitios-web">Configuración de cookies</a></li>
+                        <li>Microsoft Edge:  <a href="https://support.microsoft.com/es-es/microsoft-edge/eliminar-cookies-en-microsoft-edge-63947406-40ac-c3b8-57b9-2a946a29ae09">Configuración de cookies</a></li>
+                        <li>Safari (Apple):  <a href="https://support.apple.com/es-es/guide/safari/sfri11471/mac">Configuración de cookies</a></li>
+                    </ul>
+                    <p>Ten en cuenta que, si bloqueas algunas cookies, es posible que ciertas funciones de la web no se muestren correctamente.</p>
+                </article>
+                <article>
+                    <h2>4. Aceptación de cookies</h2>
+                    <p>Al continuar navegando por este sitio después de haber visto el aviso de cookies, aceptas el uso descrito en esta política.</p>
+                </article>
+                <article>
+                    <h2>5. Actualizaciones de la política</h2>
+                    <p>DWBA puede modificar esta Política de Cookies en cualquier momento para adaptarla a nuevas normativas o cambios en el uso de cookies. Cualquier actualización se publicará en esta misma página.</p>
+                </article>
+                <article>
+                    <h2>6. Cesión de datos a terceros</h2>
+                    <p>DWBA no comparte datos personales con terceros, salvo por obligación legal o por el uso de herramientas necesarias para la gestión del sitio web (por ejemplo, Google Analytics para estadísticas anónimas).</p>
+                </article>
+          </section>
+        </div>
+
+    )
+}
