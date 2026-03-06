215dbbce722073961c68fe5f497bc6b5429ae85a
CodeBAou <borisafou@gmail.com>
2026-03-06 20:19:48 +0100

    se añade blog y se añaden textos hotfix en inicio y diseño web

--- src/app/portafolio/page.js
+++ src/app/portafolio/page.js
@@ -1,32 +1,87 @@
 "use client";
 import React, {useState, useEffect} from 'react'
-import Data_Servicios from '../../data/page_inicio/Data_servicios_descripción_corta.json';
-import Content_carrupsel from '@/componentes/content_carrupsel/Content_carrupsel.jsx';
-
-import './servicios.css';
+import parse from 'html-react-parser';
+import './portafolio.css';
 
 export default function Page(){
 
-    return(
-        <div className="servicios">
-            <ul id="ulscroll">
-                {
-                    Data_Servicios.map((servicio,index) =>{
+    const [iconLoading, setIconLoading]         = useState(true);
+    const [postsPortafolio,setPostsPortafolio]  = useState([]);
+    let [page, setpage]                         = useState(null);// pagina actual
+    let [pagePrev, setPagePrev]                 = useState(""); // paginacion anterior
+    let [pageNext, setPageNext]                 = useState(""); // Paginacion siguiente
 
-                        return <li key={index} className="openli">
-                            <div className="carrupsel_content">
-                               <Content_carrupsel images={servicio.images_muestra_servicios} imagesMobile={servicio.imagesMobile}/>
-                            </div>
-                            <div className="title_content">
-                                <h2>{servicio.name}</h2>
-                                <div className="barra_decorativa"></div>
-                            </div>
-                        </li>
-                    })
-                }
-            </ul>
-           
+    const API_KEY           = 'AIzaSyAqOO5A5Kpr6geCb3YhEg8E3pI9VvPevTc';
+    const BLOG_ID           = '470073143992962612';
+    const params = new URLSearchParams({
+        maxResults: 10,
+        key: API_KEY
+    });
+    let url                 = `https://www.googleapis.com/blogger/v3/blogs/${BLOG_ID}/posts?${params.toString()}`;
+   
+    const fetchGetPosts = async (pagein=null) =>{
+
+     
+        // Solo añadimos el token si existe (no será null en la 2da petición)
+        if (pagein) {
+            params.append('pageToken', pagenin);
+        }
+
+        try {
+            const respuesta = await fetch(url);
+            
+            if (!respuesta.ok) {
+                throw new Error(`Error: ${respuesta.status}`);
+            }
+
+            const datos = await respuesta.json();
+          
+            // Los posts están dentro de datos.items
+            const posts      = datos.items;
+            const portafolio = posts.filter( post => post.labels.includes('portafolio')); //Filtro etiqueta portafolio
+            setPostsPortafolio(portafolio);
+            console.log("Mis entradas:", portafolio);
+            setIconLoading(false);
+            
+        } catch (error) {
+            console.error("Algo salió mal:", error);
+        }
+    }
+    
+    useEffect(()=>{
+        fetchGetPosts();
+    },[]);
+
+    return(
+        <div className="portafolio-list-content">
+            <h1>Últimos Proyectos de Diseño Web y Marketing Digital:</h1>
+            <h2>Los útimos trabajos realizados.</h2>
+          
+            {
+                (iconLoading == true)?
+                    <div className="loading-content">
+                        <img src="/image/logo_DWBA_sin_fondo.webp" className="loadingicon" alt="cargando entradas..."/> 
+                        <p>cargando...</p>
+                    </div>
+                : null
+            }
+            {
+                postsPortafolio.map((post)=>{
+                    return (
+                        <div className="content-post">
+                            {post.updated}
+                            {parse(post.content)}
+                        </div>
+                    )
+                })
+            }
+
+            <div className="paginacion-content">
+               <button onCLick={()=>fetchGetPosts(pagePrev)}>Anterior</button>
+               <button onClick={()=>fetchGetPosts(pageNext)}>Siguiente</button>
+            </div>
         </div>
+        
 
     )
 }
