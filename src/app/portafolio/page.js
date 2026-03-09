5daca660de035b30db66be3204a72f3f971802fe
CodeBAou <borisafou@gmail.com>
2026-03-09 16:59:13 +0100

    hotfix url portafolio

--- src/app/portafolio/page.js
+++ src/app/portafolio/page.js
@@ -6,86 +6,168 @@ import './portafolio.css';
 
 export default function Page(){
 
-   
-    const [iconLoading, setIconLoading]         = useState(true);
-    const [postsPortafolio,setPostsPortafolio]  = useState([]);
-    let [page, setpage]                         = useState(null);// pagina actual
-    let [pagePrev, setPagePrev]                 = useState(""); // paginacion anterior
-    let [pageNext, setPageNext]                 = useState(""); // Paginacion siguiente
-    let [post, setPost]                         = useState(0); //post actual vista
-    const [menuPortafolio, setMenuPortafolio]   = useState("display-menu-none");
-    const router = useRouter();
+    const [iconLoading, setIconLoading]           = useState(true);
+    const [postsPortafolio,setPostsPortafolio]    = useState([]);//todos las entradas
+    const [pageTokenActual, setPageTokenActual]   = useState("0");// token pagina actual
+    const [pagePrev, setPagePrev]                 = useState(null); // Token para la pagina anterior
+    const [pageNext, setPageNext]                 = useState(null); // Token para la pagina siguiente
+    const [MenuPost, menuPost]                    = useState();
+    const [menuPortafolio, setMenuPortafolio]     = useState("display-menu-none");//Estilo open/close menu de entradas
+    const router       = useRouter();
     const searchParams = useSearchParams();
-    const API_KEY           = process.env.NEXT_PUBLIC_BLOGGER_API_KEY;
-    const BLOG_ID           = process.env.NEXT_PUBLIC_BLOGGER_BLOG_ID;
+    const API_KEY      = process.env.NEXT_PUBLIC_BLOGGER_API_KEY;
+    const BLOG_ID      = process.env.NEXT_PUBLIC_BLOGGER_BLOG_ID;
    
-    const fetchGetPosts = async (pagein=null) =>{
-        const params = new URLSearchParams({
-            maxResults: 10,
-            key: API_KEY
-        });
-       
-      let url                 = `https://www.googleapis.com/blogger/v3/blogs/${BLOG_ID}/posts?${params.toString()}`;
-        // Solo añadimos el token si existe (no será null en la 2da petición)
-        if (pagein) {
-            params.append('pageToken', pagein);
+    /** Construye y devuelve la url para la peticion api, la primera peticion page token es null 
+    * El pagetoken puede ser el token anterior o el token siguiente
+    */
+    const buildApiUrl = (pagetoken=null) =>{
+
+        let params = null;
+
+        if(pagetoken==null) {
+            
+            params = new URLSearchParams({
+                maxResults: 20,
+                key: API_KEY,
+            });
+
+        }else{
+            params = new URLSearchParams({
+                maxResults:20,
+                key:API_KEY,
+                pageToken:pagetoken
+            });
         }
 
-        try {
-            const respuesta = await fetch(url);
-            
-            if (!respuesta.ok) {
-                throw new Error(`Error: ${respuesta.status}`);
+        return  `https://www.googleapis.com/blogger/v3/blogs/${BLOG_ID}/posts?${params.toString()}`
+    }
+
+    //Realiza una peticion de la pagina indicada por token, sin pageToken realiza una peticion sin el parametro de paginacion
+    const fetchGetPosts = async (pageToken=null) =>{
+        
+        let url = null;
+
+        if(searchParams.get('post') && pageToken == null){
+
+            let params = new URLSearchParams({
+                maxResults: 20,
+                key: API_KEY,
+            });
+
+            url =  `https://www.googleapis.com/blogger/v3/blogs/${BLOG_ID}/posts/${searchParams.get('post')}?${params.toString()}`;
+        
+        }else{
+            url = buildApiUrl(pageToken);
+        }
+            try {
+                let respuesta = await fetch(url);
+                if (!respuesta.ok) { throw new Error(`Error: ${respuesta.status}`); }
+                    
+                const datos = await respuesta.json();
+
+                //Guardar tokens de paginacion, Nextpagetoken, PrevPageToken ...
+                if(datos.nextPageToken) setPageNext(datos.nextPageToken);
+                if(datos.prevPageToken) setPagePrev(datos.prevPageToken);
+
+                //Obtención de la lista de post ...
+                let posts      = null;
+                let portafolio = null;
+
+                if(!datos.items){
+                    posts = [datos];
+                    
+                }else{
+                     posts = datos.items;
+                }
+
+                portafolio = posts.filter( post => post.labels?.includes('portafolio')); //Filtro etiqueta portafolio
+                router.push(`/portafolio?post=${portafolio[0].id}`, undefined, { shallow: true });
+                
+                //Guardamos la lista de posts
+                setPostsPortafolio(portafolio);
+
+                if(portafolio.length == 1)  reloadMenuPosts();
+                   
+                //Actualizamos la URL de la pagina
+                if(pageToken != null)  setPageTokenActual(pageToken);        
+                
+                //Se quita el icono "cargando..." 
+                setIconLoading(false);
+                
+            } catch (error) {
+                console.error("Algo salió mal:", error);
             }
-
-            const datos = await respuesta.json();
-          
-            // Los posts están dentro de datos.items
-            const posts      = datos.items;
-            const portafolio = posts.filter( post => post.labels.includes('portafolio')); //Filtro etiqueta portafolio
-            setPostsPortafolio(portafolio);
-            console.log("Mis entradas:", portafolio);
-            setIconLoading(false);
-            
-        } catch (error) {
-            console.error("Algo salió mal:", error);
-        }
     }
     
-    const changePostview = (e) =>{
-        let entrada = e.target.id.split("-")[1];
-        //setPost(numeroEntrada);
-        router.push(`/portafolio?page=${entrada}`, undefined, { shallow: true });
+    //Solo cambia el id de la entrada en la url 
+    const changeURLEntrada = (e) =>{
+        let entrada = e.target.id;
+        router.push(`/portafolio?post=${entrada}`, undefined, { shallow: true });
+    }
+
+    //Devuelve el post actual a partir de la url de la web
+    const getPostObj = () =>{
+        let id_entrada  = searchParams.get('post');
+        let post_Actual = postsPortafolio.filter( post => post.id == id_entrada);
+       
+        return post_Actual[0];
     }
 
     const openMenu = () =>{
-        console.log(menuPortafolio);
+       
         if(menuPortafolio === "display-menu-none"){
             setMenuPortafolio("display-menu-flex");
         }else{
             setMenuPortafolio("display-menu-none");
         }   
     }
+
+    //Cuando se hace mediante enlace solo se busca el post por id, realizamos esta peticion para cargar el menu de posts
+    const reloadMenuPosts = async() => {
+
+        let  url = buildApiUrl();
+        try {
+
+            let respuesta = await fetch(url);
+            if (!respuesta.ok) { throw new Error(`Error: ${respuesta.status}`); }   
+            const datos = await respuesta.json();
+          
+            //Guardar tokens de paginacion, Nextpagetoken, PrevPageToken ...
+            if(datos.nextPageToken) setPageNext(datos.nextPageToken);
+            if(datos.prevPageToken) setPagePrev(datos.prevPageToken);
+
+            //Obtención de la lista de post ...
+            let posts      = datos.items;
+            let portafolio = posts.filter( post => post.labels?.includes('portafolio')); //Filtro etiqueta portafolio
+            setPostsPortafolio(portafolio);
+           
+            
+        } catch (error) {
+            console.error("Algo salió mal:", error);
+        }
+    }
     
     useEffect(()=>{
-      
         fetchGetPosts();
     },[]);
 
     return(
         <div className="portafolio">
             <h1>Últimos Proyectos de Diseño Web y Marketing Digital:</h1>
-            <h2>Los útimos trabajos realizados.</h2>
-          
+            <h2>Los útimos trabajos realizados.</h2>         
             {
                 (iconLoading == true)?
                     <div className="loading-content">
                         <img src="/image/logo_DWBA_sin_fondo.webp" className="loadingicon" alt="cargando entradas..."/> 
                         <p>cargando...</p>
                     </div>
-                : null
+                : null 
+                 
             }
-           <div className="portafolio-content">
+             {
+                (iconLoading == false && searchParams.get('post')) ?
+                 <div className="portafolio-content">
 
 
                 <img className="icono-lista-portafolio" width="50px" src="/icon/portafolio-list-icon.svg" onClick={()=> openMenu()}/>
@@ -103,11 +185,10 @@ export default function Page(){
                         </div>
 
                         {(postsPortafolio.length > 0)? 
-                        <ul>
-                            
+                        <ul>  
                             {
-                                postsPortafolio.map( (post,i) => {
-                                    return <li id={post.id+"-"+i} key={post.id} onClick={(e)=>changePostview(e)}> {post.title} </li>
+                                postsPortafolio.map( (post) => {
+                                    return <li id={post.id} key={post.id} onClick={(e)=>changeURLEntrada(e)}> { (post.title)?post.title : "-"} </li>
                                 })
                             }
                         </ul>
@@ -115,7 +196,7 @@ export default function Page(){
 
                         <div className="paginacion-content">
                             <button onClick={()=>fetchGetPosts(pagePrev)}>Página Anterior</button>
-                            <span>{(page)? page : 0}</span>
+                            <span>PAGINAS</span>
                             <button onClick={()=>fetchGetPosts(pageNext)}>Siguiente Página</button>
                         </div>
                 </div>
@@ -123,19 +204,17 @@ export default function Page(){
                 <div className="portafolio-main">
                       {
                         (postsPortafolio.length > 0)? 
-                            <div key={postsPortafolio[parseInt(searchParams.get('page'))].id} className="content-post">
-                                {postsPortafolio[parseInt(searchParams.get('page'))].updated}
-                                {parse(postsPortafolio[parseInt(searchParams.get('page'))].content)}
+                            <div key={ getPostObj().id } className="content-post">
+                                <span>UPDATED:</span> {getPostObj().updated}
+                                { (getPostObj().content)? parse(getPostObj().content) : null}
                             </div>
                         : null
                       }
                    
                 </div>
            </div>
-
-          
+               : null
+             }     
         </div>
-        
-
     )
 }
