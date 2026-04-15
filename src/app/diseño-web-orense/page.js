9d9a28cedc3986ea8ddaf3934ef2da46bf13ebc5
CodeBAou <borisafou@gmail.com>
2026-04-15 13:56:06 +0200

    Correciones ortograficas en diseño-web-ourense

--- src/app/diseño-web-orense/page.js
+++ src/app/diseño-web-orense/page.js
@@ -33,7 +33,7 @@ export default function Page(){
                 }}/>
 
                 <h1 className="titlepage">Diseño Web en Ourense: Páginas Web y Soluciones </h1>
-                <h2 className="titlepage">Es diseño web, diseño de contenido y diseño para el correcto posicionamiento web.</h2>
+                <h2 className="titlepage">Es diseño web, creación de contenido y optimización para el correcto posicionamiento.</h2>
             
                 <div className="tiposWebs">
 
@@ -50,37 +50,37 @@ export default function Page(){
                         
                         <div>
                              <h3>Tipos de página web</h3>
-                             <p> Diseño web respectando la identidad de tu marca, diseñamos una web profesional que cumple con los estándares de la W3C, Google y UI/UX.</p>
+                             <p> Diseño web respetando la identidad de tu marca, diseñamos una web profesional que cumple con los estándares de la W3C, Google y UI/UX.</p>
                         </div>
                     
                         <div>
                             <h4>Diseño web de 1 página</h4>
-                            <p>Una web de una sola página como complemento a tu perfil de empresa en Google. Echa para estar presente en internet a bajo coste.</p>
+                            <p>Una web de una sola página como complemento a tu perfil de empresa en Google. Hecha para estar presente en internet a bajo coste.</p>
                         </div>
                        
                         <div>
-                            <h4>Diseño web corporativa</h4>
+                            <h4>Diseño web corporativo</h4>
                             <p>Una web corporativa, muestras tu empresa de forma profesional, te vendes y cuentas con un blog para mostrar contenido útil a tus clientes.</p>
                         </div>
                         
                         <div>
                             <h4>Diseño web ecommerce</h4>
-                            <p>Una tienda online con todas las funcionalidades necesarias para vender y con la posibilidad de soluccionar problemas desarrollando solucciones específicas. </p>
+                            <p>Una tienda online con todas las funcionalidades necesarias para vender y con la posibilidad de solucionar problemas desarrollando funcionalidades a medida. </p>
                         </div>
                        
                         <div>
                             <h4>Diseño web servicios</h4>
-                            <p>Una web para ofrecer un servicio o autmatizar procesos como reservas online entre otros.</p>
+                            <p>Una web para ofrecer un servicio o automatizar procesos como reservas online entre otros.</p>
                         </div>
                        
                         <div>
-                            <h4>Diseño de aplicación web</h4> 
-                            <p>Como una aplicación de escritorio o móvil, pero servida como web.</p>
+                            <h4>Diseño de aplicaciones web</h4> 
+                            <p>Como una aplicación de escritorio o móvil, accesible a través del navegador.</p>
                         </div>
                        
                         <div>
                             <h4>Diseño a medida</h4>
-                            <p>Solucciones muy específicas según tus necesidades, incluyendo solucciones con IA implementada.</p>
+                            <p>Soluciones muy específicas según tus necesidades, incluyendo soluciones con IA implementada.</p>
                         </div>  
                 </div>
 
@@ -97,8 +97,9 @@ export default function Page(){
 
             <section className="tipos-tecnologias-content">
 
-                <h3>No dependemos de una tecnología, elegimos el stack tecnológico más adecuado para el proyecto y para ti.</h3>
-                <p>A la hora de crear un proyecto podemos usar dos tecnologías diferentes, CMS WordPress y React para cubrir necesidades diferentes.</p>
+                <h3>No dependemos de una sola tecnología: buscamos el stack más adecuado para el proyecto y para ti</h3>
+                <p>Seleccionamos las herramientas que mejor cubran las necesidades específicas de cada desarrollo:</p>
+                
                 <div className="esquema-tecnologias">
                     <div>
                         <div className="point"></div>
@@ -154,11 +155,11 @@ export default function Page(){
                     </figure>   
 
                     <div>
-                        <p><b className="definicion-clave">Estrategia y Requerimientos:</b> Trabajamos de forma telématica ofreciendo reuniones online para entender el proyecto.</p>
-                        <p><b className="definicion-clave">Prototipado y Diseño:</b> Una vez tenemos los requerimientos empezamos la etapa de desarrollo, prototipo y diseño, creando una idea visual para revisar antes de producción.</p>
+                        <p><b className="definicion-clave">Estrategia y Requerimientos:</b> Trabajamos de forma telemática ofreciendo reuniones online para entender el proyecto.</p>
+                        <p><b className="definicion-clave">Prototipado y Diseño:</b> Una vez tenemos los requerimientos, empezamos la etapa de desarrollo, prototipo y diseño, creando una idea visual para revisar antes de producción.</p>
                         <p><b className="definicion-clave">Desarrollo y Producción:</b> Cuando estamos de acuerdo con el diseño, empezamos la producción.</p>
-                        <p><b className="definicion-clave">Revisión y Control de Calidad:</b> Acordamos una reunión con la web y la revisamos, asegurando que todo está bien cumpliendo lo acordado.</p>
-                        <p><b className="definicion-clave">Entrega y Soporte:</b> Una vez nos des tú conformidad, se factura mensualmente o puntualmente dependiendo del servicio y lo acordado.</p>  
+                        <p><b className="definicion-clave">Revisión y Control de Calidad:</b> Acordamos una reunión para ver la web y la revisamos, asegurando que todo está bien y cumple lo acordado.</p>
+                        <p><b className="definicion-clave">Entrega y Soporte:</b> Una vez nos des tu conformidad, se factura mensual o puntualmente, dependiendo del servicio y lo acordado.</p> 
                     </div>
                     
                 </div>
@@ -166,7 +167,7 @@ export default function Page(){
 
             <section className="portafolio">
 
-                <h2>Últimos diseños Web WordPress </h2>    
+                <h2>Últimos diseños web en WordPress</h2>  
                     
                 <div>
                     <MuestraVerticalDwba title="Tienda" link="https://charcuteriauria.es/" src="/image/portafolio/miniatura_web_uriahnos.webp" Width="200px"/>
@@ -178,18 +179,18 @@ export default function Page(){
             </section>
 
            <section className="beneficios">
-                <h2>Beneficios al tener una página web, bien cuidada, actualizada y optimizada.</h2>
+                <h2>Beneficios al tener una página web bien cuidada actualizada y optimizada.</h2>
                 <article>
                     <h3>Presencia profesional y credibilidad</h3>
                     <p>Una página web es la carta de presentación de tu organización, transmite confianza, seriedad y mejora tu imagen frente a clientes o empresas.</p>
                 </article>
                 <article>
                     <h3>Estás visible las 24 horas</h3>
-                    <p>Con una web, tu negocio está disponible todo el día, todos los días. Así es posible mostrarte a los usuarios que buscan tus servicio en cualquier momento.</p>
+                    <p>Con una web, tu negocio está disponible todo el día, todos los días. Así es posible mostrarte a los usuarios que buscan tus servicios en cualquier momento.</p>
                 </article>
                  <article>
                     <h3>Generas nuevos clientes y ventas</h3>
-                    <p>Una web bien diseñada capta contacto, reservas o ventas automáticas. Es como tener un comercial activo las 24h.</p>
+                    <p>Una web bien diseñada capta contactos, reservas o ventas automáticas. Es como tener un comercial activo las 24h.</p>
                 </article>
                  <article>
                     <h3>Amplía la visibilidad en Google</h3>
@@ -224,14 +225,14 @@ export default function Page(){
 
                <article>
                     <h2>Contacto</h2>
-                    <p>Si tienes alguna duda contacta sin compromiso y pregúntanos.</p>
+                    <p>Si tienes alguna duda, contacta sin compromiso y pregúntanos.</p>
                     <p>Desde Ourense, colaboramos en proyectos de cualquier lugar de forma telemática.</p>
                     <Link href="/#contacto">CONTACTO</Link>
                </article>
 
                <article>
                     <h2>Otros servicios para tu página web</h2>
-                    <p>Quizás no necesitas un diseño web, pero contamos con otros servicios para mejorarla y mantenerla.</p>
+                    <p>Quizá no necesitas un diseño web, pero contamos con otros servicios para mejorarla y mantenerla.</p>
                     <Link href="/#servicio-de-diseño-web">VER SERVICIOS</Link>
                </article>
 
