dfe0193b6b83ac0cf5b6a6527d1581cc5697385c
CodeBAou <borisafou@gmail.com>
2026-04-29 01:23:26 +0200

    divisor-apartado-web

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Content_section_description from '@/componentes/content_section_descripti
 import Data_servicos from '@/data/servicios.json';
 import Section_header from '@/componentes/section_header/Section_header.jsx';
 import Content_section_contacto from '@/componentes/content_section_contacto/Content_section_contacto';
-import Content_section_oferta from '@/componentes/callAction_diseño_web/CallActionConsigueWeb';
+import DivisorApartadoWeb from '@/componentes/divisor-apartado-web/DivisorApartadoWeb.jsx';
 import Content_section_footer from '@/componentes/content_section_footer/Content_section_footer';
 
 export default function Page(){
@@ -22,7 +22,11 @@ export default function Page(){
                         <Section_header/>
                     
                         <Layout_section_inicio key={`section-OFERTA`}>
-                                <Content_section_oferta />
+                                <DivisorApartadoWeb title="DWBA | ¿Que servicios ofrecemos?">
+                                    <img src="/icon/desarrollo.svg" width="50px" height="50px"/>
+                                    <img src="/icon/seo.svg" width="50px" height="50px"/>
+                                    <img src="/icon/crecimiento.svg" width="50px" height="50px"/>
+                                </DivisorApartadoWeb>
                         </Layout_section_inicio>
                         
                         {/*SectionS*/}
