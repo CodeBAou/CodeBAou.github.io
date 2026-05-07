8549a6f9bd96aa6a893b820c6b6f0afe43bbc45c
CodeBAou <borisafou@gmail.com>
2026-05-07 19:02:10 +0200

    Se completa la pagina diseño-web-wordpress-ourense

--- src/app/dise%C3%B1o-web-orense/page.js
+++ src/app/dise%C3%B1o-web-orense/page.js
@@ -1,9 +1,8 @@
 "use client";
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import Link from 'next/link'
 import './diseñoweb.css';
 import MuestraVerticalDwba from '@/componentes/muestra-vertical-dwba/MuestraVerticalDwba.jsx';
-import ImageDwba from '@/componentes/Image-dwba/Image.jsx';
 import Section_diseño_web_responsive from '@/componentes/section_diseño_web_responsive/Section_diseño_web_responsive';
 import Section_diseño_web_apisGoogle from '@/componentes/section_diseño_web_apisGoogle/Section_diseño_web_apisGoogle';
 import SectionTipos from '@/componentes/section_tipo_webs/SectionTipos';
@@ -24,20 +23,7 @@ export default function Page(){
                 alt="La interfaz para el diseño de páginas web, captura durante el diseño web de una página para una bodega."
                 title="Servicio de diseño web desde Ourense"
                 subtitle="Nuestros diseños webs están pensados para que te encuentren en Google, ofrecer la funcionalidad que quieras y convertir usuarios en clientes"
-                />
-
-            <section className="portafolio">
-                    <h3>Algunos diseños webs</h3>
-                    <div className="webs">
-                        <MuestraVerticalDwba title="Tienda" link="https://charcuteriauria.es/" src="/image/portafolio/miniatura_web_uriahnos.webp" Width="200px"/>
-                        <MuestraVerticalDwba title="Sitio" link="https://habitarqestudio.com/" src="/image/portafolio/miniatura_web_habitarq.webp" Width="200px"/>
-                    </div>
-
-                    <div className="enlaces">
-                        <p> En nuestro portafolio mostramos notas de desarrollador y comentamos proyectos, funcionalidades y soluciones que te pueden interesar para incluir en tu página web. </p>
-                        <Link href="/portafolio" target="_blank"> Ver Portafolio </Link>
-                    </div>
-            </section>
+            />
 
             <section className="maxheight">
                 <SectionTipos/>
@@ -70,6 +56,19 @@ export default function Page(){
                 <Section_diseño_web_apisGoogle/>
             </section>
 
+            <section className="portafolio">
+                <h3>Algunos diseños webs</h3>
+                <div className="webs">
+                    <MuestraVerticalDwba title="Tienda" link="https://charcuteriauria.es/" src="/image/portafolio/miniatura_web_uriahnos.webp" Width="200px"/>
+                    <MuestraVerticalDwba title="Sitio" link="https://habitarqestudio.com/" src="/image/portafolio/miniatura_web_habitarq.webp" Width="200px"/>
+                </div>
+
+                <div className="enlaces">
+                    <p> En nuestro portafolio mostramos notas de desarrollador y comentamos proyectos, funcionalidades y soluciones que te pueden interesar para incluir en tu página web. </p>
+                    <Link href="/portafolio" target="_blank"> Ver Portafolio </Link>
+                </div>
+            </section>
+
             <section className="maxheight">
                 <TextChild title="¿QUIERES UN DISEÑO WEB EN WORDPRESS?" parrafo="Un diseño web para wordpress.">
                      <p>Si necesitas una web autogestionable con CMS WordPress, podemos diseñar temas o webs completas, bloques Gutenberg para incluir apartados separados, bloques Gutenberg para integrar APIs de Google o bloques basados en animaciones CSS.</p>
@@ -90,7 +89,6 @@ export default function Page(){
             </section>
 
             <section className="contacto-content">
-
                <article>
                     <h2>Contacto</h2>
                     <p>Si tienes alguna duda, contacta sin compromiso y pregúntanos.</p>
---
+++ src/app/dise%C3%B1o-web-wordpress-ourense/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Servicio de diseño web WordPress | DWBA',
+  description: 'Diseño web para WordPress, diseño de bloques y plugins. Servicio desde Ourense.',
+  keywords: 'diseño web wordpress Ourense, diseño de pagina web wordpress, venta de páginas web wordpress, diseñador WordPress',
+  robots: 'index, follow',
+};
+
+export default function RootLayout({ children }) {
+  return (
+       <>
+        {children}
+       </>
+  )
+}
